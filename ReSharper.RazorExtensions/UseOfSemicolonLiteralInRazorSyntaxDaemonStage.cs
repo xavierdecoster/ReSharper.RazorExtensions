dbca54d34ea61b75b3df27cc02d99d2a5c412480
Xavier Decoster <xavier.decoster@gmail.com>
2013-04-03 21:32:29 +0200

    initial commit

---
+++ ReSharper.RazorExtensions/UseOfSemicolonLiteralInRazorSyntaxDaemonStage.cs
@@ -0,0 +1,28 @@
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Razor.CSharp;
+using JetBrains.ReSharper.Psi.Tree;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReSharper.RazorExtensions
+{
+    [DaemonStage(StagesBefore = new[] { typeof(LanguageSpecificDaemonStage) })]
+    public class UseOfSemicolonLiteralInRazorSyntaxDaemonStage : IDaemonStage
+    {
+        public IEnumerable<IDaemonStageProcess> CreateProcess(IDaemonProcess process, IContextBoundSettingsStore settings, DaemonProcessKind processKind)
+        {
+            IFile psiFile = process.SourceFile.GetNonInjectedPsiFile<RazorCSharpLanguage>();
+            if (psiFile == null)
+                return Enumerable.Empty<IDaemonStageProcess>();
+
+            return new[] { new UseOfSemicolonLiteralInRazorSyntaxDaemonProcess(process) };
+        }
+
+        public ErrorStripeRequest NeedsErrorStripe(IPsiSourceFile sourceFile, IContextBoundSettingsStore settingsStore)
+        {
+            return ErrorStripeRequest.STRIPE_AND_ERRORS;
+        }
+    }
+}
---
+++ ReSharper.RazorExtensions/UseOfSemicolonLiteralInRazorSyntaxHighlighting.cs
@@ -0,0 +1,50 @@
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Psi.Html.Impl.Tree;
+using JetBrains.ReSharper.Psi.Razor.CSharp.Mvc;
+using JetBrains.ReSharper.Psi.Razor.Parsing;
+using JetBrains.ReSharper.Psi.Tree;
+using ReSharper.RazorExtensions;
+
+[assembly: RegisterConfigurableSeverity(UseOfSemicolonLiteralInRazorSyntaxHighlighting.SeverityId,
+  null,
+  HighlightingGroupIds.CodeSmell,
+  "Suspicious usage of semicolons found in Razor syntax.",
+  "Use of literal semicolon in Razor views can cause undesired rendering of semicolon in HTML pages.",
+  Severity.WARNING,
+  false)]
+
+namespace ReSharper.RazorExtensions
+{
+  [ConfigurableSeverityHighlighting(SeverityId, RazorCSharpMvcLanguage.Name, OverlapResolve = OverlapResolveKind.WARNING)]
+  public class UseOfSemicolonLiteralInRazorSyntaxHighlighting : IHighlighting
+  {
+    private readonly HtmlToken<IRazorTokenNodeTypes> _htmlToken;
+
+    public const string SeverityId = "UseOfSemicolonLiteralInRazorSyntax";
+
+    public UseOfSemicolonLiteralInRazorSyntaxHighlighting(HtmlToken<IRazorTokenNodeTypes> htmlToken)
+    {
+      _htmlToken = htmlToken;
+    }
+
+    public string ToolTip
+    {
+      get { return "Use of literal semicolon in Razor views can cause undesired rendering of semicolon in HTML pages."; }
+    }
+
+    public string ErrorStripeToolTip
+    {
+      get { return "Suspicious usage of semicolons found in Razor syntax."; }
+    }
+
+    public int NavigationOffsetPatch
+    {
+      get { return 0; }
+    }
+
+    public bool IsValid()
+    {
+      return _htmlToken != null && _htmlToken.IsValid();
+    }
+  }
+}
