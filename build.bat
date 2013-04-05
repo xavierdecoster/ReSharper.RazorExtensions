048f1d15b34d4a6562b537f03bb3558b4aff3656
Xavier Decoster <xavier.decoster@gmail.com>
2013-04-05 22:08:56 +0200

    prepping for R# 8.0

--- build.bat
+++ build.bat
@@ -17,8 +17,8 @@ if "%nuget%" == "" (
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ReSharper.RazorExtensions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
  
 mkdir Build
-mkdir Build\v8.0
+mkdir Build\v7.1
+copy ReSharper.RazorExtensions.v71\bin\%config%\*.dll Build\v7.1
+copy ReSharper.RazorExtensions.v71\bin\%config%\*.pdb Build\v7.1
 
-%nuget% pack "ReSharper.RazorExtensions\ReSharper.RazorExtensions.nuspec" -symbols -verbosity detailed -o Build\v8.0 -Version %version% -p Configuration="%config%"
-copy ReSharper.RazorExtensions\bin\%config%\*.dll Build\v8.0
-copy ReSharper.RazorExtensions\bin\%config%\*.pdb Build\v8.0
+%nuget% pack ".nuget\ReSharper.RazorExtensions.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
