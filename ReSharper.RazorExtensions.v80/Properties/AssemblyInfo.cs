167b701a9de618fe8570794596d213cd2d443ced
Xavier Decoster <xavier.decoster@gmail.com>
2013-04-27 13:54:08 +0200

    removing actionsxml stuff (fixed refs)

--- ReSharper.RazorExtensions.v80/Properties/AssemblyInfo.cs
+++ ReSharper.RazorExtensions.v80/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
-using JetBrains.Application.PluginSupport;
 using System.Reflection;
+using JetBrains.ActionManagement;
+using JetBrains.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,6 +16,8 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ActionsXml("ReSharper.RazorExtensions.v80.Actions.xml")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("ReSharper.RazorExtensions")]
