86563f71554b6123922d1d093fda7c4464b18226
xavierdecoster <xavier.decoster@gmail.com>
2013-11-14 21:11:32 +0100

    fixed myget build (added pkg restore -pre instructions for v8.1 R# SDK EAP)

--- build.bat
+++ build.bat
@@ -14,8 +14,13 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+REM Package restore
+%nuget% install ReSharper.RazorExtensions.v81\packages.config -OutputDirectory %cd%\packages -NonInteractive -Prerelease
+
+REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ReSharper.RazorExtensions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
- 
+
+REM Post-Build
 mkdir Build
 mkdir Build\v7.1
 copy ReSharper.RazorExtensions.v71\bin\%config%\*.dll Build\v7.1
@@ -27,4 +32,5 @@ mkdir Build\v8.1
 copy ReSharper.RazorExtensions.v81\bin\%config%\*.dll Build\v8.1
 copy ReSharper.RazorExtensions.v81\bin\%config%\*.pdb Build\v8.1
 
+REM Package creation
 %nuget% pack ".nuget\ReSharper.RazorExtensions.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
